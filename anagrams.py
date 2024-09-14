735c39765223b7769dc11f31da3b2d429a9eb042
julinaa <76502829+julinaa@users.noreply.github.com>
2024-09-14 14:13:15 +0100

    Add files via upload

---
+++ anagrams.py
@@ -0,0 +1,37 @@
+class Anagrams(object):
+    def __init__(self,a,b):
+        self.word1 = a
+        self.word2 = b
+
+    def match(self):
+        if len(self.word1) != len(self.word2):
+            return False
+        lst1=[0]*26
+        lst2=[0]*26
+        for i in self.word1:
+            lst1[ord('a')-ord(i)] +=1
+        for j in self.word2:
+            lst2[ord('a')-ord(j)] +=1
+        if lst1 != lst2:
+            return False
+        return True
+def anagrams(a,b):
+    if len(a) != len(b):
+        return False
+    lst1=[0]*26
+    lst2=[0]*26
+    for i in a:
+        lst1[ord('a')-ord(i)] +=1
+    for j in b:
+        lst2[ord('a')-ord(j)] +=1
+    if lst1 != lst2:
+        return False
+    return True
+
+
+"""word1=input("a:")
+word2=input("b")
+print(anagrams(word1,word2))"""
+
+test1=Anagrams("bus","gub")
+print(test1.match())
